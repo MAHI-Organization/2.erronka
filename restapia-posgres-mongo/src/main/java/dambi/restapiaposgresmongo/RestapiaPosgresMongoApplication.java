d61ae52b1cdce0678e5d15ac88c2bbb081a6e57b
Markel Rodriguez <rodriguez.markel@uni.eus>
2023-01-16 09:53:09 +0100

    restapia sortu

---
+++ restapia-posgres-mongo/src/main/java/dambi/restapiaposgresmongo/RestapiaPosgresMongoApplication.java
@@ -0,0 +1,13 @@
+package dambi.restapiaposgresmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiaPosgresMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiaPosgresMongoApplication.class, args);
+	}
+
+}
---
+++ restapia-posgres-mongo/src/test/java/dambi/restapiaposgresmongo/RestapiaPosgresMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package dambi.restapiaposgresmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiaPosgresMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
