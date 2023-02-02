f368d1fff5171c44feb737a47f54267821b793aa
Markel Rodriguez <rodriguez.markel@uni.eus>
2023-02-02 09:36:08 +0100

    Api kontsumitzailea 1.Bertsioa
    Gure servidorearen datuak jasotzen eta gordetzen ditu

--- kontsumitzailea/src/main/java/dambi/App.java
+++ kontsumitzailea/src/main/java/dambi/App.java
@@ -1,39 +1,74 @@
 package dambi;
 
+
+
+import com.mongodb.client.MongoClient;
+
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class App 
-{
+public class App {
+    
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
+        MongoClient mongoClient = null;
+        try {
+            
+            // Conectar a la base de datos MongoDB
+            mongoClient = MongoClients.create("mongodb://192.168.65.14:27017");
         
-        try{
-            URL url = new URL("http://localhost:8080/api/allLangileak");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
+            MongoDatabase database = mongoClient.getDatabase("erronka2");
+            MongoCollection<Document> collection = database.getCollection("partida");
 
-            int responseCode = conn.getResponseCode();
-            if(responseCode != 200){
-                throw new RuntimeException("Errore bat gertatu da: " + responseCode);
+            // Realizar una solicitud GET a la API REST de Swagger
+            URL url = new URL("http://192.168.65.11:8080/api/allPartidak");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            // Leer la respuesta de la API y convertirla a un objeto JSON
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
             }
-            else{
-                StringBuilder informationString = new StringBuilder();
-                Scanner scanner = new Scanner(url.openStream());
+            in.close();
+            JSONArray json = new JSONArray(response.toString());
 
-                while(scanner.hasNext()){
-                    informationString.append(scanner.nextLine());   
-                }
-                scanner.close();
-
-                System.out.println(informationString);
+            for(Object obj :json){
+                 Document document = new Document(((JSONObject) obj).toMap());
+                collection.insertOne(document);
             }
-        }
-        catch(Exception e){
-            e.printStackTrace();
+                
+            // Insertar los datos del objeto JSON en la base de datos MongoDB
+           
+
+            // Registrar la hora de la inserción en un archivo de texto
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String dateString = sdf.format(new Date());
+            FileWriter writer = new FileWriter("log.txt", true);
+            writer.write(dateString + " - Los datos se han insertado correctamente en la base de datos MongoDB.\n");
+            writer.close();
+
+            // Cerrar la conexión con la base de datos MongoDB
+            mongoClient.close();
+
+            System.out.println("Los datos se han insertado correctamente en la base de datos MongoDB y se ha registrado la hora en el archivo de texto.");
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error: " + e.getMessage());
         }
     }
-       
-   
 }
