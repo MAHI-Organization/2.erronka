be9a2b667ec2de21b776f4f6431f6226151e9005
Iker Gallastegui <gallastegui.iker@uni.eus>
2023-02-03 08:37:19 +0100

    Web Orria
    Orri printzipalaren diseinua eginda

---
+++ MAHI_LeaderBoard/MAHI_LeaderBoard/ViewComponents/JokoakViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using MAHI_LeaderBoard.Services;
+using Microsoft.AspNetCore.Mvc;
+
+public class JokoakViewComponent : ViewComponent
+{
+    private readonly IJokoaService _jokoaService;
+    public JokoakViewComponent(IJokoaService jokoaService)
+    {
+        _jokoaService = jokoaService;
+    }
+    public async Task<IViewComponentResult> InvokeAsync(int id)
+    {
+        return View(await _jokoaService.GetErabiltzailea(id));
+    }
+}
+
