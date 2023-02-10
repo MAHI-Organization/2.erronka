68185e3e5724b25db962a6c9040f21a8a076b7f6
Hodei Olivas <olivas.hodei@uni.eus>
2023-02-10 08:49:03 +0100

    Estilo batzuk

--- MAHI_LeaderBoard/MAHI_LeaderBoard/Controllers/FirstGameController.cs
+++ MAHI_LeaderBoard/MAHI_LeaderBoard/Controllers/FirstGameController.cs
@@ -1,12 +1,94 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MAHI_LeaderBoard.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System.Net.Http.Headers;
 
 namespace MAHI_LeaderBoard.Controllers
 {
+    
     public class FirstGameController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+        
+        [Route("guardar_datos")]
+        [HttpPost]
+
+        public async Task<IActionResult> GuardarDatos(Inkesta inkestak)
+        {
+            // Verifica que los datos del cuestionario se hayan proporcionado correctamente
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    // Establece la URL de la API
+                    client.BaseAddress = new Uri("http://localhost:8080/swagger-ui/index.html");
+
+                    // Establece el tipo de contenido que se enviará en la petición
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    // Envía los datos del cuestionario a la API
+                    HttpResponseMessage response = await client.PostAsJsonAsync("api/guardar_datos", inkestak);
+
+                    // Verifica si la petición se ha completado correctamente
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return Ok();
+                    }
+                    else
+                    {
+                        return StatusCode(500, response.ReasonPhrase);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
+       /* public IActionResult GuardarDatos(Inkesta inkestak)
+        {
+            // Verifica que los datos del cuestionario se hayan proporcionado correctamente
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Conéctate a MongoDB y guarda los datos del cuestionario
+            try
+            {
+                // Crea una instancia de MongoClient para conectarte a la base de datos
+                var client = new MongoClient("mongodb://localhost:27017");
+
+                // Obtén una referencia a la base de datos
+                var database = client.GetDatabase("erronka2");
+
+                // Obtén una referencia a la colección de cuestionarios
+                var cuestionariosCollection = database.GetCollection<Inkesta>("inkestak");
+
+                // Inserta los datos del cuestionario en la colección de cuestionarios
+                cuestionariosCollection.InsertOne(inkestak);
+
+                // Cierra la conexión a MongoDB
+                client = null;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+            return Ok();
+        }  */
+    
+
+
---
+++ MAHI_LeaderBoard/MAHI_LeaderBoard/Models/Inkesta.cs
@@ -0,0 +1,16 @@
+﻿namespace MAHI_LeaderBoard.Models
+{
+    public class Inkesta
+
+    {
+        public string Erabiltzailea { get; set; }
+        public int jokoaID { get; set; }
+        public string jokoarenIzena { get; set; }
+        public string lehenengoGaldera { get; set; }
+        public string bigarrenGaldera { get; set; }
+
+        public string hirugarrenGaldera { get; set; }
+
+        public string laugarrenGaldera { get; set; }
+    }
+}
